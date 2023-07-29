5d1ea3151bd6df089855105fcd905dc996f67fe1
bellaborrego38 <bellaborrego38@yahoo.com>
2023-07-29 12:38:31 -0500

    Had to create this new repo, transferred everything over from other repo. was having node_module issues because of file placement and build. Trying a new method to deploy with heroku

---
+++ my-portfolio/src/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+    {
+      title: "Sentinetic",
+      subtitle: "MERN Stack",
+      description:
+        "Most recent collaboration- This is a sentiment analysis application. We used Twitter API to fetch the tweets and Chat GPT's open AI API for the analysis. The pie chart demonstrates the functionality by displaying whether the specific tweets searched are positive, negative, or neutral.",
+      image: "./Screen Shot 2023-07-24 at 9.42.06 PM.png",
+      link: "https://sentinetic-987fae0a7fa7.herokuapp.com/",
+    },
+    {
+      title: "Task hub",
+      subtitle: "MySQL/Sequelize/HandlebarsJS",
+      description:
+        "Collaboration- This application provides users with a platform where they can share their daily tasks and goals with others who can see their progress. Knowing that others can see their achievements or lack thereof creates a sense of responsibility and accountability.",
+      image:"./Screen Shot 2023-07-13 at 9.58.29 AM.png",
+      link: "https://taskhub.herokuapp.com/",
+    },
+    {
+      title: "Dinner Genie",
+      subtitle: "HTML/CSS/JavaScript",
+      description:
+        "Collaboration- This application encourages users to create their own meals at home. All you have to do is type up to 3 ingredients that you have in your fridge at the moment and dinner genie makes your dinner wishes come true by generating recipes for you based off of the ingredients you typed in. If you are looking for a surprise cocktail recipe, click the genie's lamp!",
+      image: "./dinner-genie.png",
+      link: "https://landywandy.github.io/Dinner-Genie/",
+    },
+    {
+      title: "ORM E-Commerce backend",
+      subtitle: "Node.js/Express/MySQL",
+      description:
+        "This backend application was created for an internet retail company to compete with other e-commerce companies using the latest technologies. This is a video demonstrating the functionality of the backend created. ",
+      image: "./hero-it-readiness-back-end.jpg",
+      link: "https://drive.google.com/file/d/18RAUkOM7FW8EuErpxtpz42nPPfB6o73a/view",
+    },
+    {
+      title: "JavaScript password generator",
+      subtitle: "JavaScript/HTML/CSS",
+      description:
+        "This application generates a password after prompting the user with questions of what they may want the password to be. If the acceptance criteria is met, the password is successfully generated.",
+      image: "./javascript password generator.png",
+      link: "https://bellaborrego38.github.io/Javascript-password-generator/",
+    },
+    {
+      title: "Work day schedular",
+      subtitle: "JavaScript/HTML/CSS",
+      description:
+        "This application is for users with a busy workday! There are 12 time blocks that represent each hour from 5AM-5PM. Events can be saved for each block hour. The past blocks automatically turn gray, the present block is always red, and the future blocks are green.",
+      image: "./maxresdefault.jpg",
+      link: "https://bellaborrego38.github.io/API-workday-scheduler/",
+    },
+  ];
+  
+  
